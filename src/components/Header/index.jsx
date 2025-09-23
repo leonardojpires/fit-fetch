b4ea67478b7f69f5c7521d06676c569e9e3ed189
Leonardo Pires <165698194+leonardojpires@users.noreply.github.com>
2025-09-23 11:07:51 +0100

    Responsive menu header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,49 @@
+import './index.css';
 import { Link } from "react-router-dom";
 import { CiLogin } from "react-icons/ci";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Header() {
+    const [ showMenu, setShowMenu ] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
     return (
-        <nav className="flex justify-evenly items-center py-4">
+        <header className="flex justify-between items-center py-4 px-10 lg:justify-evenly">
+
             <div>
                 <Link to="/">
                     <span className="text-3xl font-bold">
-                        <img src="/img/logos/ff_logo.svg" alt="Fit Fetch" width="200px" />
+                        <img src="/img/logos/ff_logo.svg" alt="Fit Fetch" width="200px" className="select-none pointer-none" />
                     </span>
                 </Link>
             </div>
-            <div className="flex flex-row justify-center items-center gap-10">
+
+            <nav 
+                className={`nav ${ showMenu ? 'flex responsive-nav' : '' }`}
+                onClick={toggleMenu}
+            >
                 <Link to="/" className="font-headline nav-link underline-hover">HOME</Link>
                 <Link to="/treinos" className="font-headline nav-link underline-hover">TREINOS</Link>
                 <Link to="/nutricao" className="font-headline nav-link underline-hover">NUTRIÇÃO</Link>
                 <Link to="/contacto" className="font-headline nav-link underline-hover">CONTACTO</Link>
-            </div>
-            <div>
+
                 <a href="" class="font-headline auth-button">
                     ENTRAR
                     <CiLogin />
                 </a>
+            </nav>
+
+            <div 
+                className='menu-button'
+                onClick={toggleMenu}                    
+            >
+                <GiHamburgerMenu />
             </div>
-        </nav>
+
+        </header>
     )
 }
 
