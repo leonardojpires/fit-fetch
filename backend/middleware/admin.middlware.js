b9f6c42c48e5243ec61f746559700dce3d23508c
Leonardo Pires <165698194+leonardojpires@users.noreply.github.com>
2025-10-28 12:00:47 +0000

    created checkAdmin middleware

---
+++ backend/middleware/admin.middlware.js
@@ -0,0 +1,10 @@
+const checkAdmin = (req, res, next) => {
+    const user = req.user;
+
+    if (user && user.role === 'admin') {
+        return res.status(403).json({ message: "Acesso negado. Apenas administradores podem realizar esta operação" });
+    }
+    next();
+}
+
+export default checkAdmin;
