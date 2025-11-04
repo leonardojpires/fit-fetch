a2b617ce0f97ed6bc61766465a8e67ebee703125
Leonardo Pires <165698194+leonardojpires@users.noreply.github.com>
2025-11-04 15:29:54 +0000

    created admin middleware

---
+++ backend/middleware/verifyAdmin.js
@@ -0,0 +1,15 @@
+const verifyAdmin = async (req, res, next) => {
+    const allowedRole = 'admin';
+
+    try {
+        if (req.user.role !== allowedRole) {
+            return res.status(403).json({ message: "Acesso negado!" });
+        }
+        next();
+    } catch(err) {
+        console.error("Erro ao verificar as permissões de administrador: ", err);
+        return res.status(500).json({ message: "Erro interno do servidor." });
+    }
+}
+
+export default verifyAdmin;
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import verifyFirebaseToken from "../middleware/verifyFirebaseToken.middleware.js";
 import attachUserFromDB from '../middleware/attachUserFromDB.middleware.js';
 import UserController from "../controllers/UserController.js";
+import verifyAdmin from './../middleware/verifyAdmin.js';
 
 
 const userRouter = Router();
 
 userRouter.post("/sync", verifyFirebaseToken, UserController.syncUser);
-userRouter.get("/me", verifyFirebaseToken, attachUserFromDB, UserController.getCurrentUser);
-userRouter.get("/all", verifyFirebaseToken, attachUserFromDB, UserController.getAllUsers);
+userRouter.get("/me", verifyFirebaseToken, attachUserFromDB, verifyAdmin, UserController.getCurrentUser);
+userRouter.get("/all", verifyFirebaseToken, attachUserFromDB, verifyAdmin, UserController.getAllUsers);
 
 export default userRouter;
