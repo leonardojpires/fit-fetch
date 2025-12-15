c8520110e0f8886a0fa364f9f6a8c54657a59b1b
leonardojpires <leonardojpires2006@gmail.com>
2025-12-15 15:03:05 +0000

    created UserContext - navbar user info now updates simultaneously to profile view when changing profile information

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { UserProvider } from './contexts/UserContext.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </StrictMode>,
 )
